449c1419155b19317672aaf2509fd812c4d2fe3e
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-04-02 09:15:28 -0300

    feat: Retorno do UserId no Token
    feat: Retorno do UserId no Token

--- 4-InfraData/2-JWT/TokenService.cs
+++ 4-InfraData/2-JWT/TokenService.cs
@@ -16,18 +16,22 @@ namespace _4_InfraData._2_JWT
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                new Claim(ClaimTypes.Name, user.Name.ToString()),
-                new Claim(ClaimTypes.Role, user.Role.ToString())
+            new Claim(ClaimTypes.Name, user.Name.ToString()),
+            new Claim(ClaimTypes.Role, user.Role.ToString()),
+            new Claim("userId", user.Id.ToString()) // ✅ Adiciona o ID do usuário
                 }),
                 Expires = DateTime.UtcNow.AddHours(2),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
+
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
+
     }
 }
