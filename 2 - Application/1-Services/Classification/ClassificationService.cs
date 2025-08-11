9433e081c987297d19602b52a7f5df45f79faa53
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-08-11 17:21:41 -0300

    Update ClassificationService.cs

--- 2 - Application/1-Services/Classification/ClassificationService.cs
+++ 2 - Application/1-Services/Classification/ClassificationService.cs
@@ -1643,7 +1643,7 @@ namespace _2___Application._1_Services
 
                 var outrosResultadosOperacionais = totalizerResponses
                     .SelectMany(t => t.Classifications)
-                    .FirstOrDefault(c => c.Name == "Outros Resultados Operacionais")?.Value ?? 0;
+                    .FirstOrDefault(c => c.Name == "Outros  Resultados Operacionais")?.Value ?? 0;
 
                 if (despesasOperacionais != null)
                     despesasOperacionais.TotalValue = despesasOperacionais.TotalValue + despesasDepreciacao + outrosResultadosOperacionais; // comentando o - outros resultados pois e a conta que falta para fechar o resultado, ams no excel fornecido nao conta com esse campo
