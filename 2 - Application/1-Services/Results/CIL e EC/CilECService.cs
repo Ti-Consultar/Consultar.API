ed1fc680be4ea5e19e1d436aeffaa4813dfc4680
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-08-12 15:41:27 -0300

    Update CilECService.cs

--- 2 - Application/1-Services/Results/CIL e EC/CilECService.cs
+++ 2 - Application/1-Services/Results/CIL e EC/CilECService.cs
@@ -146,7 +146,7 @@ namespace _2___Application._1_Services.Results.CIL_e_EC
                         RealizavelLongoPrazo = realizavelLongoPrazo,
                         ExigivelALongoPrazoOperacional = exigivelLongoPrazo,
                         AtivosFixos = ativosFixos,
-                        CapitalInvestidoLiquido = patrimonioLiquido,
+                        CapitalInvestidoLiquido = patrimonioLiquido * -1,
 
                     },
                     EstruturaDeCapital = new ECResponseDto
