3e050893874b85c6ce4f819568c9c5321e635756
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-08-13 15:46:03 -0300

    Update CashFlowService.cs

--- 2 - Application/1-Services/CashFlow/CashFlowService.cs
+++ 2 - Application/1-Services/CashFlow/CashFlowService.cs
@@ -206,7 +206,7 @@ namespace _2___Application._1_Services.CashFlow
                     OutrosPassivosOperacionais = variacaoOutrosPassivosOperacionais,
                     FluxoDeCaixaOperacional = fluxoCaixaOperacional,
                     AtivoNaoCirculante = variacaoAtivoNaoCirculante,
-                    VariacaoInvetimento = variacaoInvestimento,
+                    VariacaoInvetimento = previousMonth.VariacaoInvetimento,
                     VariacaoImobilizado = imobilizado,
                     FluxoDeCaixaLivre = fluxoCaixaLivre,
                     CaptacoesAmortizacoesFinanceira = emprestimoEFinanciamento,
