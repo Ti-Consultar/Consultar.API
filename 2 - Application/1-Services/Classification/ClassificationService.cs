bb351e9a38327066529aaf6e3724b1b7d0a31881
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-08-22 14:34:02 -0300

    Update ClassificationService.cs

--- 2 - Application/1-Services/Classification/ClassificationService.cs
+++ 2 - Application/1-Services/Classification/ClassificationService.cs
@@ -1229,10 +1229,11 @@ namespace _2___Application._1_Services
                     decimal totalAtivo = ativoFinanceiro + ativoOperacional + ativoFixo;
 
 
-                    //var depreciacao = totalizerResponses.FirstOrDefault(a => a.Name == "Depreciação / Amort. Acumulada");
+                    var depreciacao = totalizerResponses.FirstOrDefault(a => a.Name == "Depreciação / Amort. Acumulada");
 
+                    decimal sinalInvertido = depreciacao.TotalValue * -1;
 
-                    //depreciacao.TotalValue = depreciacao.TotalValue * -1; // Inverte o sinal da depreciação
+                    depreciacao.TotalValue = sinalInvertido;
 
                     return new MonthPainelContabilRespone
                     {
