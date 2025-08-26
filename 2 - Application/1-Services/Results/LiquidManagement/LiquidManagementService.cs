52bf475a11221204e1b48a793328090d8cf4925a
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-08-26 15:50:40 -0300

    Update LiquidManagementService.cs

--- 2 - Application/1-Services/Results/LiquidManagement/LiquidManagementService.cs
+++ 2 - Application/1-Services/Results/LiquidManagement/LiquidManagementService.cs
@@ -322,7 +322,7 @@ namespace _2___Application._1_Services.Results
 
                 var receitaMensal = monthDRE?.Totalizer.FirstOrDefault(t => t.Name == "(=) Receita Líquida de Vendas")?.TotalValue ?? 0;
 
-                decimal geracaoCaixa = (receitaMensal != 0 ? fluxoDeCaixaOperacional / receitaMensal : 0) * 100;
+                decimal geracaoCaixa = ((receitaMensal != 0 ? fluxoDeCaixaOperacional / receitaMensal : 0) * 100) * -1;
                 decimal aumentoReducaoFluxoCaixa = margemEbitda != 0 ? geracaoCaixa - margemEbitda : 0;
 
                 grossCashFlow.Add(new GrossCashFlowResponseDto
