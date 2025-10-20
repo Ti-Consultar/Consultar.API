7f3d94b7582e97d1cdf1815646fb9ece54483755
Ti-Consultar <ti@consultarconsultoria.com.br>
2025-10-20 14:48:20 -0300

    feat: tabela de budget

---
+++ Database/dbo/Tables/Budget.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Budget (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    AccountPlansId INT NOT NULL,
+    DateMonth INT NOT NULL,
+    DateYear INT NOT NULL,
+    DateCreate DATE NOT NULL,
+    CONSTRAINT FK_Budget_Accountplan 
+        FOREIGN KEY (AccountPlansId) REFERENCES AccountPlans(Id) ON DELETE CASCADE
+);
